fc5277fc7b884710a9901fd87eb795027eade80e
Mark H Smith <markhassellsmith@gmail.com>
2026-03-11 11:58:31 -0400

    Refactor: Make architecture extensible for multiple fract
    al types- Add FractalType enum with Newton, Mandelbrot, and Hailstone options
    - Standardize all render methods to accept (int w, int h, Bitmap targetBmp)
    - Add switch statement in Canvas_Paint for fractal type selection
    - Extract magic numbers to named constants for maintainability
    - Remove unused instance fields (deltax, deltay, w, h, wp, hp)
    - Remove unused coordinate transformation methods
    - Remove all commented-out code
    - Fix Hailstone method to accept Graphics parameter and use proper disposal
    - Remove intrusive MessageBox popup after render completion
    - Convert string concatenation to string interpolation in Hailstone
    
    Ready for easy addition of new fractal visualizations.

--- WinFormsFractal/Canvas.cs
+++ WinFormsFractal/Canvas.cs
@@ -10,30 +10,41 @@ namespace WinFormsFractal
 {
     public partial class Canvas : Form
     {
+        #region FractalTypeEnum
+
+        private enum FractalType
+        {
+            Newton,
+            Mandelbrot,
+            Hailstone
+        }
+
+        #endregion FractalTypeEnum
+
         #region CanvasVariables
 
-        //------------------ bitmap dimensions, functional range dimensions //
-        private static int maxhoriz = 2000;
+        private const int MaxHorizontal = 2000;
+        private const int MaxVertical = 2000;
+        private const int XWidth = 4;
+        private const int YHeight = 4;
 
-        private static int maxvert = 2000;
-        private static int xwidth = 4;
-        private static int yheight = 4;
-        private double deltax = (double)xwidth / (double)maxhoriz;   // divide the xwidth up into maxhoriz parts
-        private double deltay = (double)yheight / (double)maxvert;    // divide the yheight up into maxvert parts
+        private const int MaxIterations = 1200;
+        private const double Tolerance = 1e-10;
 
-        //------------------these are variables in my paperwork //
-        private static int w = maxhoriz;
+        private const double MandelbrotEscapeRadius = 1000000.0;
+        private const int MandelbrotMaxIterations = 512;
+        private const int MandelbrotColorOffset = 270;
+        private const int MandelbrotColorMultiplier = 19;
+        private const int MandelbrotColorModulo = 359;
 
-        private static int h = maxvert;
-        private static int wp = xwidth;
-        private static int hp = yheight;
+        private const int NewtonHueSpread = 17;
 
-        //------------------iterations, tolerance, and bitmap object //
-        // increased iterations and tighter tolerance for finer isobar detail
-        private int maxiterations = 1200;
+        private const int HailstoneColorSpread = 517;
 
-        private double tolerance = 1e-10;  // tighter tolerance for better root convergence
-        private Bitmap bmp = new Bitmap(maxhoriz, maxvert);
+        private FractalType currentFractal = FractalType.Newton;
+        private int maxiterations = MaxIterations;
+        private double tolerance = Tolerance;
+        private Bitmap bmp = new Bitmap(MaxHorizontal, MaxVertical);
 
         // render guard to avoid repeated expensive renders and message boxes
         private bool imageRendered = false;
@@ -49,50 +60,53 @@ namespace WinFormsFractal
 
         #region FractalMethods
 
-        private void MandelBrot()
+        private void Mandelbrot(int w, int h, Bitmap targetBmp)
         {
-            for (int px = 0; px < maxhoriz; px++)
+            double localDeltaX = (double)XWidth / (double)w;
+            double localDeltaY = (double)YHeight / (double)h;
+            for (int px = 0; px < w; px++)
             {
-                for (int py = 0; py < maxvert; py++)
+                for (int py = 0; py < h; py++)
                 {
-                    double x = (double)(-xwidth) / 2.0 + deltax * px;
-                    double y = (double)(-yheight) / 2.0 + deltay * py;
+                    double x = (double)(-XWidth) / 2.0 + localDeltaX * px;
+                    double y = (double)(-YHeight) / 2.0 + localDeltaY * py;
                     Complex c = new(x, y);
                     Complex zstart = Complex.Zero;
-                    Complex znext = Functions.FMandelbrot(zstart, c);  // Mandelbrot interation here
-                    int color = 0;
-                    while (znext.Magnitude < 1000000.0 && color < 512)
+                    Complex znext = Functions.FMandelbrot(zstart, c);
+                    int iterations = 0;
+                    while (znext.Magnitude < MandelbrotEscapeRadius && iterations < MandelbrotMaxIterations)
                     {
                         znext = Functions.FMandelbrot(znext, c);
-                        color++;
-                    }
-                    if (znext.Magnitude >= 1000000.0) //  if the magnitude blew up
-                    {
-                        color = (((color % 359) + 270) * 19) % 359;  // color based on how many iterations it required to blow up
-                    }
-                    else  // it did not blow up in 512 iterations
-                    {
-                        color = 0;  // color it black for the Mandelbrot stable points set
+                        iterations++;
                     }
 
-                    // the number of iterations to blow up (magnitude diverges)   is being plotted in the color scale
-                    //bmp.SetPixel(px,  py, Color.FromArgb(ColorPalettes.Spectrum72[color].red, ColorPalettes.Spectrum72[color].green, ColorPalettes.Spectrum72[color].blue));
-                    bmp.SetPixel(px, py, Color.FromArgb(ColorPalettes.Spectrum360[color].red, ColorPalettes.Spectrum360[color].green, ColorPalettes.Spectrum360[color].blue));
+                    Color color;
+                    if (znext.Magnitude >= MandelbrotEscapeRadius)
+                    {
+                        int colorIndex = (((iterations % MandelbrotColorModulo) + MandelbrotColorOffset) * MandelbrotColorMultiplier) % MandelbrotColorModulo;
+                        var cs = ColorPalettes.Spectrum360[colorIndex];
+                        color = Color.FromArgb(cs.red, cs.green, cs.blue);
+                    }
+                    else
+                    {
+                        color = Color.FromArgb(0, 0, 0);
+                    }
+
+                    targetBmp.SetPixel(px, py, color);
                 }
             }
         }
 
         private void NewtonsMethod(int w, int h, Bitmap targetBmp)
         {
-            // map pixel coordinates into the function domain using deltax/deltay and xwidth/yheight
-            double localDeltaX = (double)xwidth / (double)w;
-            double localDeltaY = (double)yheight / (double)h;
+            double localDeltaX = (double)XWidth / (double)w;
+            double localDeltaY = (double)YHeight / (double)h;
             for (int px = 0; px < w; px++)
             {
                 for (int py = 0; py < h; py++)
                 {
-                    double x = (double)(-xwidth) / 2.0 + localDeltaX * px;
-                    double y = (double)(-yheight) / 2.0 + localDeltaY * py;
+                    double x = (double)(-XWidth) / 2.0 + localDeltaX * px;
+                    double y = (double)(-YHeight) / 2.0 + localDeltaY * py;
 
                     Complex zstart = new Complex(x, y);
                     Complex znext = zstart - Functions.F(zstart) / Functions.FP(zstart);
@@ -115,7 +129,7 @@ namespace WinFormsFractal
                     {
                         double arg = Math.Atan2(znext.Imaginary, znext.Real);
                         int hueFromArg = (int)Math.Round(((arg / (2.0 * Math.PI)) + 0.5) * 360.0) % 360;
-                        int hueIndex = (hueFromArg + iterations * 17) % 360;
+                        int hueIndex = (hueFromArg + iterations * NewtonHueSpread) % 360;
                         var cs = ColorPalettes.Spectrum360[(hueIndex + 360) % 360];
                         color = Color.FromArgb(cs.red, cs.green, cs.blue);
                     }
@@ -125,30 +139,25 @@ namespace WinFormsFractal
             }
         }
 
-        private void HailStone(int startingX, int startingY)
+        private void Hailstone(int startingX, int startingY, Graphics graphics)
         {
-            Pen myDrawingPen = new Pen(Color.Red);
-            myDrawingPen.Width = 4.0F;
-            //TextRenderer.DrawText(e.Graphics, "This is some text.", this.Font,
-            //        new Point(1000, 1000), Color.White, Color.SteelBlue, TextFormatFlags.Default);
-            colorstruct cs;
+            using Pen drawingPen = new Pen(Color.Red) { Width = 4.0F };
+
             for (int index = 1; index <= maxiterations; index++)
             {
-                cs = ColorPalettes.Spectrum360[(index * 517) % 360];
-                myDrawingPen.Color = Color.FromArgb(cs.red, cs.green, cs.blue);
+                var cs = ColorPalettes.Spectrum360[(index * HailstoneColorSpread) % 360];
+                drawingPen.Color = Color.FromArgb(cs.red, cs.green, cs.blue);
                 int nextX = Functions.FHailStoneNextX(startingX, startingY);
                 int nextY = Functions.FHailStoneNextY(startingX, startingY);
 
-                // label the points using the four values
-                string startinglabel = "(" + Convert.ToString(index) + ", " + Convert.ToString(startingX) + ", " + Convert.ToString(startingY) + ")";
-                string endinglabel = "(" + Convert.ToString(index + 1) + ", " + Convert.ToString(nextX) + ", " + Convert.ToString(nextY) + ")";
-                TextRenderer.DrawText(g, startinglabel, this.Font,
+                string startinglabel = $"({index}, {startingX}, {startingY})";
+                string endinglabel = $"({index + 1}, {nextX}, {nextY})";
+                TextRenderer.DrawText(graphics, startinglabel, this.Font,
                      new Point(startingX, startingY), Color.White, Color.SteelBlue, TextFormatFlags.Default);
-                TextRenderer.DrawText(g, endinglabel, this.Font,
+                TextRenderer.DrawText(graphics, endinglabel, this.Font,
                      new Point(nextX, nextY), Color.White, Color.SteelBlue, TextFormatFlags.Default);
 
-                // then draw the connecting line
-                g.DrawLine(myDrawingPen, new PointF(startingX, startingY), new PointF(nextX, nextY));
+                graphics.DrawLine(drawingPen, new PointF(startingX, startingY), new PointF(nextX, nextY));
                 startingX = nextX;
                 startingY = nextY;
                 if (nextX == 1 && nextY == 1) break;
@@ -157,42 +166,17 @@ namespace WinFormsFractal
 
         #endregion FractalMethods
 
-        #region CoordinateTranformations
-
-        private Point TransformPointToDomain(Point P)
-        {
-            Point Q = new Point(0, 0);
-            Q.X = (wp / w) * P.X - w / 2;
-            Q.Y = (-hp / h) * P.Y - h / 2 + hp;
-            return Q;
-        }
-
-        private Point TransformPointToPixel(Point P)
-        {
-            Point Q = new Point(0, 0);
-            Q.X = (w / wp) * (P.X + w / 2);
-            Q.Y = h - h * h / 2 / hp - (h / hp) * P.Y;
-            return Q;
-        }
-
-        #endregion CoordinateTranformations
-
         #region CanvasMethods
 
-        public Canvas() // constructor
+        public Canvas()
         {
             InitializeComponent();
-            // ensure the display panel fills the form and scales the image
             panel1.Dock = DockStyle.Fill;
-            // use Zoom so the image preserves aspect ratio; we'll render an image matching the panel aspect ratio
             panel1.BackgroundImageLayout = ImageLayout.Zoom;
-            //this.BackColor = Color.Black;
-            //this.MouseClick += mouseClick;
         }
 
         private void Canvas_Paint(object sender, PaintEventArgs e)
         {
-            // start background render once
             if (imageRendered || renderingInProgress) return;
 
             renderingInProgress = true;
@@ -200,27 +184,40 @@ namespace WinFormsFractal
             {
                 try
                 {
-                    // choose a render size that matches the panel aspect ratio to avoid letterboxing when using Zoom
                     int panelW = Math.Max(1, panel1.ClientSize.Width);
                     int panelH = Math.Max(1, panel1.ClientSize.Height);
                     double panelRatio = panelW / (double)panelH;
-                    double baseRatio = maxhoriz / (double)maxvert;
+                    double baseRatio = MaxHorizontal / (double)MaxVertical;
                     int targetW, targetH;
                     if (panelRatio >= baseRatio)
                     {
-                        // panel is wider: use full internal height, increase width to match aspect
-                        targetH = maxvert;
-                        targetW = Math.Max(1, (int)Math.Round(maxvert * panelRatio));
+                        targetH = MaxVertical;
+                        targetW = Math.Max(1, (int)Math.Round(MaxVertical * panelRatio));
                     }
                     else
                     {
-                        // panel is taller: use full internal width, increase height to match aspect
-                        targetW = maxhoriz;
-                        targetH = Math.Max(1, (int)Math.Round(maxhoriz / panelRatio));
+                        targetW = MaxHorizontal;
+                        targetH = Math.Max(1, (int)Math.Round(MaxHorizontal / panelRatio));
                     }
 
                     var renderBmp = new Bitmap(targetW, targetH);
-                    NewtonsMethod(targetW, targetH, renderBmp);
+
+                    switch (currentFractal)
+                    {
+                        case FractalType.Newton:
+                            NewtonsMethod(targetW, targetH, renderBmp);
+                            break;
+                        case FractalType.Mandelbrot:
+                            Mandelbrot(targetW, targetH, renderBmp);
+                            break;
+                        case FractalType.Hailstone:
+                            using (var g = Graphics.FromImage(renderBmp))
+                            {
+                                g.Clear(Color.Black);
+                                Hailstone(targetW / 2, targetH / 2, g);
+                            }
+                            break;
+                    }
 
                     Directory.CreateDirectory(@"C:\Temp");
                     renderBmp.Save(@"C:\Temp\fractal.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
@@ -233,8 +230,7 @@ namespace WinFormsFractal
                             // dispose old bmp
                             bmp?.Dispose();
                             bmp = renderBmp;
-                            panel1.BackgroundImage = bmp; // Zoom preserves aspect; image matches panel aspect so it will fill
-                            MessageBox.Show("image finished");
+                            panel1.BackgroundImage = bmp;
                             imageRendered = true;
                         }
                         catch (Exception ex)
@@ -256,7 +252,6 @@ namespace WinFormsFractal
                     });
                 }
             });
-            //SaveSpectrum360AsHtml(Path.ChangeExtension(path, "html"), cols, swatchSize);
         }
 
         #endregion CanvasMethods
@@ -266,20 +261,12 @@ namespace WinFormsFractal
         private void Canvas_MouseDown(object sender, MouseEventArgs e)
         {
             pdown = e.Location;
-            //MessageBox.Show("Mouse down");
         }
 
         private void Canvas_MouseUp(object sender, MouseEventArgs e)
         {
             pup = e.Location;
-            //MessageBox.Show("Mouse up");
         }
-
-        //private void mouseClick(object sender, MouseEventArgs e)
-        //{
-        //    if (e.Button == MouseButtons.Left)
-        //        Trace.WriteLine("Mouse clicked");
-        //}
 
         #endregion MouseMethods
     }
